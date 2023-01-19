817554bc17c9978960ad892b4c4a016f8e3b655f
diez.a@northeastern.edu <diez.a@northeastern.edu>
2023-01-19 16:55:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPractice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonPractice = findViewById(R.id.buttonPractice);
+
+        buttonPractice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toPracticeActivity = new Intent(MainActivity.this, PracticeActivity.class);
+                startActivity(toPracticeActivity);
+            }
+        });
+    }
+}
